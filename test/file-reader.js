e88179a79faa4dde5915d3dd80b80653b7af9d74
Mikołaj <mikolaj@Mikoajs-MacBook-Pro.local>
2018-03-10 19:09:57 +0100

    Tests for file-reader

---
+++ test/file-reader.js
@@ -0,0 +1,40 @@
+import { expect } from "chai";
+import countLettersInFile, { countLettersInString, readFilePromise, getUniqueLettersFromString } from '../util/file-reader';
+
+describe ("File Reader", () => {
+    
+    it ("should return string from txt file", () => {
+        return readFilePromise("testfile.txt").then(string => {
+            expect(string).to.be.a("string");
+        });
+    });
+
+    it ("should return array of unique letters in string", () => {
+        return getUniqueLettersFromString("AAABBC").then(uniqueLetters => {
+            expect(uniqueLetters).to.be.deep.equal(["A", "B", "C"]);
+        });
+    });
+
+    it ("should return array of object with value and frequency of letter in string", () => {
+        return countLettersInString("AAABBCC").then(letters => {
+            expect(letters).to.deep.equal([
+                { value: "A", frequency: 3 },
+                { value: "B", frequency: 2 },
+                { value: "C", frequency: 1 },
+            ]);
+        });
+    });
+
+    it ("should return array of object with value and frequeny of letter from txt file", () => {
+        return countLettersInFile("testfile.txt").then(letters => {
+            expect(letters).to.be.deep.equal([
+                { value: "A", frequency: 45 },
+                { value: "D", frequency: 16 },
+                { value: "B", frequency: 13 },
+                { value: "C", frequency: 12 },
+                { value: "E", frequency: 9 },
+                { value: "F", frequency: 5 },
+            ]);
+        });
+    });
+});
---
+++ util/file-reader.js
@@ -0,0 +1,11 @@
+import fs from "fs";
+
+export const readFilePromise = (path) => {};
+
+export const getUniqueLettersFromString = (string) => {};
+
+export const countLettersInString = (string, letters = []) => {};
+
+const countLettersInFile = (path) => {};
+
+export default countLettersInFile;
