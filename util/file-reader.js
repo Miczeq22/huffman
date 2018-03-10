9f92eec7e892811cb6ffbe97a9b5574d24d46036
Mikołaj <mikolaj@Mikoajs-MacBook-Pro.local>
2018-03-10 19:17:22 +0100

    getUniqueLettersFromString function

--- util/file-reader.js
+++ util/file-reader.js
@@ -12,7 +12,23 @@ export const readFilePromise = (path) => {
     });
 };
 
-export const getUniqueLettersFromString = (string) => {};
+export const getUniqueLettersFromString = (string) => {
+    return new Promise((resolve, reject) => {
+        if (typeof string !== "string" || string.length === 0) {
+            reject(Error("String can't be empty"));
+        } else {
+            const letters = [];
+
+            for (let i = 0; i < string.length; i++) {
+                if (string[i] !== string[i + 1] && string[i + 1] !== "\n") {
+                    letters.push(string[i]);
+                }
+            }
+
+            resolve(letters);
+        }
+    });
+};
 
 export const countLettersInString = (string, letters = []) => {};
 
