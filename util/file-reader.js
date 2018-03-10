0413a6acdeee0fd7e2e532d3ab4a0afb7b165b71
Mikołaj <mikolaj@Mikoajs-MacBook-Pro.local>
2018-03-10 19:11:50 +0100

    readFilePromise function

--- util/file-reader.js
+++ util/file-reader.js
@@ -1,6 +1,16 @@
 import fs from "fs";
 
-export const readFilePromise = (path) => {};
+export const readFilePromise = (path) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, "utf8", (error, data) => {
+            if (error) {
+                reject(Error("File not found."));
+            } else {
+                resolve(data);
+            }
+        });
+    });
+};
 
 export const getUniqueLettersFromString = (string) => {};
 
